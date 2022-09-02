8ae730f6d398791cab1c991683a324e2534e795d
Sunny Singh <94648812+Sunny4800@users.noreply.github.com>
2022-09-02 20:00:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,24 @@ const outputbox = document.querySelector(".output");
 console.log(luckyno)
 function checknumberislucky(){
     
-    if(luckyno===0){
-        outputbox.innerText = "Please Enter a Valid Number"; 
-    }
-    else
-    {
+//     if(luckyno===0){
+//         outputbox.innerText = "Please Enter a Valid Number"; 
+//     }
+//     else
+//     {
+//     }
     const dob = dateOfBirth.value;
     const sum = calculate(dob);
+        if(sum&&dob)
         if(sum%luckyno===0){
             outputbox.innerText ="Your Birthday is Lucky 😊";
         }
         else{
             outputbox.innerText ="Your Birthday is not Lucky 😥";
         } 
-    }
+    else
+        outputbox.innerText = "Please Enter a Valid Number"; 
+
 
 }
 
