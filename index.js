65d0c707589882d96155e199615e1b9df9f34ddf
Sunny Singh <94648812+Sunny4800@users.noreply.github.com>
2022-09-02 18:39:48 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const dateOfBirth = document.querySelector("#dateofbirth");
+const luckyno = document.querySelector("#luckynumber");
+const checkbutton = document.querySelector("#checknumber");
+const outputbox = document.querySelector("#output");
+
+
+function checknumberislucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculate(dob);
+    function check(sum,luckyno){
+        if(sum%luckyno ===0){
+        outputbox.innerText ="Your Birthday is Lucky 😊";
+    }
+    else{
+        outputbox.innerText ="Your Birthday is not Lucky 😊";
+    }
+}}
+
+function calculate(dob){
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for(let index=0;index<dob.length;index++){
+        sum = sum + Number(dob.charAt(index));
+    }
+    return sum;
+} 
+
+checkbutton.addEventListener('click', checknumberislucky);
